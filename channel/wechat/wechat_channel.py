5faff244e00cfb42258351984a068ba338dae4a8
Tomfry <114723861+robin202208@users.noreply.github.com>
2024-03-09 07:08:22 -0300

    Update wechat_channel.py

--- channel/wechat/wechat_channel.py
+++ channel/wechat/wechat_channel.py
@@ -209,6 +209,7 @@ class WechatChannel(ChatChannel):
     # 统一的发送函数，每个Channel自行实现，根据reply的type字段发送不同类型的消息
     def send(self, reply: Reply, context: Context):
         receiver = context["receiver"]
+        time.sleep(5) 
         if reply.type == ReplyType.TEXT:
             itchat.send(reply.content, toUserName=receiver)
             logger.info("[WX] sendMsg={}, receiver={}".format(reply, receiver))
